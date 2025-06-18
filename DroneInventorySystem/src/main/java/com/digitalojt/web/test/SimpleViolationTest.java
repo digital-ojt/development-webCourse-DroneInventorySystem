1d3f69da3655941ba1eac98fc7cd3e36d75ee2f8
madaoz1974 <u2721.1.1618@gmail.com>
2025-06-18 20:19:19 +0900

    modified:   .gitignore 	modified:   DroneInventorySystem/src/main/java/com/digitalojt/web/test/SimpleViolationTest.java

--- DroneInventorySystem/src/main/java/com/digitalojt/web/test/SimpleViolationTest.java
+++ DroneInventorySystem/src/main/java/com/digitalojt/web/test/SimpleViolationTest.java
@@ -1,7 +1,7 @@
 package com.digitalojt.web.test;
 
 // 意図的な違反: クラス名がファイル名と異なる
-public class simple_test {
+public class SimpleViolationTest {
 
 	// 意図的な違反: フィールド名規則違反
 	private String WRONG_FIELD_NAME;
