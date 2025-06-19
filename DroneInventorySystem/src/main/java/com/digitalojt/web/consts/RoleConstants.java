e4f75b0ca4925031e92f98162b00e3489974e60a
madaoz1974 <u2721.1.1618@gmail.com>
2025-06-19 21:09:49 +0900

    TEST: pre-commit result output fix test

--- DroneInventorySystem/src/main/java/com/digitalojt/web/consts/RoleConstants.java
+++ DroneInventorySystem/src/main/java/com/digitalojt/web/consts/RoleConstants.java
@@ -11,4 +11,6 @@ public class RoleConstants {
 	public static final String ROLE_USER = "USER";
 	// 管理者IDのname属性
 	public static final String ADMIN_ID = "adminId";
+	// テスト用: 複数の空行を追加してCheckstyle違反を発生
+
 }
